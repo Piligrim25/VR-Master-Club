c453fa38bd4f1c9b3289e6816a6193d516b7c49e
piligrim25 <my.paradise.ts@mail.ru>
2018-09-22 23:32:19 +0300

    floating menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
+import { StickyHeaderComponent } from './sticky-header/sticky-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StickyHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/script.js
+++ src/app/script.js
@@ -1,13 +1,40 @@
-$(function() {
-    $('.toggles button').click(function(){
-     var get_id = this.id;
-     var get_current = $('.posts .' + get_id);
-   
-     $('.post').not(get_current).hide(500);
-     get_current.show(500);
-    });
-   
-    $('#showall').click(function() {
-     $('.post').show(500);
-    });
-   });
+$(function () {
+  $('.toggles button').click(function () {
+    var get_id = this.id;
+    var get_current = $('.posts .' + get_id);
+
+    $('.post').not(get_current).hide(500);
+    get_current.show(500);
+  });
+
+  $('#showall').click(function () {
+    $('.post').show(500);
+  });
+});
+
+
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 3
+      },
+      600: {
+        items: 3
+      },
+      800: {
+        items: 4
+      },
+      1000: {
+        items: 5,
+        loop: false
+      },
+      1300: {
+        items: 8,
+        loop: false
+      }
+    }
+  })
+});
